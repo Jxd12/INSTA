d12c874fd06c779a2702b6b9edb502a76066a036
hlw207 <211250207@smail.nju.edu.cn>
2023-12-02 14:40:37 +0800

    SKNetwork finished

--- INSTA/DKGenerator.py
+++ INSTA/DKGenerator.py
@@ -1,5 +1,10 @@
+import torch
+import torch.nn as nn
 class DKGenerator():
-    def __init__(self):
+    def __init__(self, c, k):
+        self.SKk = k
+        self.SKconv= nn.Conv2d(c, k ** 2, kernel_size=1)
+        self.SKbn = nn.BatchNorm2d(k ** 2)  # BN层
 
     def forward(self,data):
         """
@@ -32,7 +37,19 @@ class DKGenerator():
         Spatial Kernel Network 何 TODO
         Args:
             data:
-
+             b, c, h, w
+             b为样本总数
+             c为通道数
+             h为高度
+             w为宽度
         Returns:
 
         """
+        batch_size, in_channels, height, width = data.size()
+        Gsp = self.SKconv(data)
+        Gsp = self.SKbn(Gsp)
+        Gsp = Gsp.view(batch_size, height, width, self.SKk, self.SKk)
+        Gsp = Gsp.unsqueeze(1)
+        Gsp = Gsp.expand(batch_size, in_channels, *Gsp.shape[2:])
+
+        return Gsp
