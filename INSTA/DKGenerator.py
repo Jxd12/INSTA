91d3ea61148249ee79cd181702a461045b19b79f
hlw207 <211250207@smail.nju.edu.cn>
2023-12-23 15:25:01 +0800

    寄！

--- INSTA/DKGenerator.py
+++ INSTA/DKGenerator.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 class DKGenerator():
-    def __init__(self, c, k):
+    def __init__(self, c, k, spatial_size):
         self.SKk = k
         self.SKconv= nn.Conv2d(c, k ** 2, kernel_size=1)
-        self.SKbn = nn.BatchNorm2d(k ** 2)  # BN层
+        self.SKbn = nn.BatchNorm2d(spatial_size ** 2)  # BN层
 
     def forward(self,data):
         """
@@ -50,11 +50,12 @@ class DKGenerator():
         # 提取b，c，h，w
         Gsp = self.SKconv(data)
         # 通过1*1卷积层，将通道数变为k**2
-        Gsp = self.SKbn(Gsp)
+        Gsp = Gsp.flatten(-2).transpose(-1, -2)
+        size = Gsp.size()
         # 对Gsp进行归一化处理
-        Gsp = Gsp.view(batch_size, height, width, self.SKk, self.SKk)
+        Gsp = Gsp.view(size[0], -1, self.SKk, self.SKk)
+        Gsp = self.SKbn(Gsp)
         # 对Gsp进行变形，变成h，w，k，k
-        Gsp = Gsp.unsqueeze(1)
-        Gsp = Gsp.expand(batch_size, in_channels, *Gsp.shape[2:])
+        Gsp = Gsp.flatten(-2)
         # 对Gsp进行广播操作，生成c，h，w，k，k
-        return Gsp
+        return Gsp.unsqueeze(-3)
